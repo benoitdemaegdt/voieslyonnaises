a6de6b697348aa08097de64511d2939e26291bb0
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-09-11 19:52:25 +0200

    replace action panel by map panel

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -64,33 +64,11 @@
               </div>
             </section>
 
-            <!-- Actions panel -->
+            <!-- Map panel -->
             <section aria-labelledby="quick-links-title">
               <div class="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
                 <h2 class="sr-only" id="quick-links-title">Quick links</h2>
-                <div v-for="(action, actionIdx) in actions" :key="action.name" :class="[actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '', actionIdx === 1 ? 'sm:rounded-tr-lg' : '', actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '', actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '', 'relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-cyan-500']">
-                  <div>
-                    <span :class="[action.iconBackground, action.iconForeground, 'rounded-lg inline-flex p-3 ring-4 ring-white']">
-                      <!-- <component :is="action.icon" class="h-6 w-6" aria-hidden="true" /> -->
-                      <Icon name="mdi:bike" class="h-6 w-6" aria-hidden="true"></Icon>
-                    </span>
-                  </div>
-                  <div class="mt-8">
-                    <h3 class="text-lg font-medium">
-                      <a :href="action.href" class="focus:outline-none">
-                        <!-- Extend touch target to entire panel -->
-                        <span class="absolute inset-0" aria-hidden="true" />
-                        {{ action.name }}
-                      </a>
-                    </h3>
-                    <p class="mt-2 text-sm text-gray-500">Doloribus dolores nostrum quia qui natus officia quod et dolorem. Sit repellendus qui ut at blanditiis et quo et molestiae.</p>
-                  </div>
-                  <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                    </svg>
-                  </span>
-                </div>
+                <p>Map : work in progress</p>
               </div>
             </section>
           </div>
@@ -121,7 +99,7 @@
                     </ul>
                   </div>
                   <div class="mt-6">
-                    <a href="#" class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> View all </a>
+                    <a href="#" class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Tout voir </a>
                   </div>
                 </div>
               </div>
@@ -182,15 +160,6 @@ const { path } = useRoute()
 const { data: voie } = await useAsyncData(`test-${path}`, () => {
   return queryContent().where({ _path: path, _type: 'json' }).findOne()
 })
-
-const actions = [
-  { icon: '', name: 'Request time off', href: '#', iconForeground: 'text-teal-700', iconBackground: 'bg-teal-50' },
-  { icon: '', name: 'Benefits', href: '#', iconForeground: 'text-purple-700', iconBackground: 'bg-purple-50' },
-  { icon: '', name: 'Schedule a one-on-one', href: '#', iconForeground: 'text-sky-700', iconBackground: 'bg-sky-50' },
-  { icon: '', name: 'Payroll', href: '#', iconForeground: 'text-yellow-700', iconBackground: 'bg-yellow-50' },
-  { icon: '', name: 'Submit an expense', href: '#', iconForeground: 'text-rose-700', iconBackground: 'bg-rose-50' },
-  { icon: '', name: 'Training', href: '#', iconForeground: 'text-indigo-700', iconBackground: 'bg-indigo-50' },
-]
 
 const announcements = [
   {
