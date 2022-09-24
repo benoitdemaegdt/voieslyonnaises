b2e4ec061735df3830a0880e2c0a86f901ce0361
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-09-24 17:44:15 +0200

    add link to all lines in header

--- components/Header.vue
+++ components/Header.vue
@@ -29,6 +29,31 @@
           >
             {{ navItem.name }}
           </NuxtLink>
+
+          <Popover class="relative" v-slot="{ open }">
+            <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2']">
+              <span>Lignes</span>
+              <Icon name="mdi:chevron-down" :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true"></Icon>
+            </PopoverButton>
+            <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
+              <PopoverPanel class="absolute left-1/2 z-10 mt-3 w-screen max-w-md -translate-x-1/2 transform px-2 sm:px-0">
+                <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                  <div class="relative grid grid-cols-2 sm:grid-cols-4 gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                    <NuxtLink v-for="voie in voies" :key="voie.line" :href="voie._path" class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50">
+                      <div class="flex-shrink-0">
+                        <div
+                          class="h-10 w-10 rounded-full flex items-center justify-center text-white font-bold"
+                          :style="`background-color: ${voie.color}`"
+                        >
+                        {{ voie.line }}
+                        </div>
+                      </div>
+                    </NuxtLink>
+                  </div>
+                </div>
+              </PopoverPanel>
+            </transition>
+          </Popover>
         </PopoverGroup>
         <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
           <NuxtLink
@@ -59,9 +84,9 @@
         >
           <div class="pt-5 pb-6 px-5">
             <div class="flex items-center justify-between">
-              <div>
+              <NuxtLink to="/" @click="close()">
                 <img class="h-8 w-auto" src="~/assets/logo.png" alt="logo voies lyonnaises" />
-              </div>
+              </NuxtLink>
               <div class="-mr-2">
                 <PopoverButton
                   class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500"
@@ -88,13 +113,19 @@
             </div>
           </div>
           <div class="py-6 px-5 space-y-6 bg-gray-50">
-            <div class="grid grid-cols-2 gap-y-4 gap-x-8">
-              <NuxtLink
-                to="/"
-                @click="close()"
-                class="ml-3 text-base font-medium text-gray-900 hover:text-gray-700"
-              >
-                Accueil
+            <div class="ml-3 text-base font-medium text-gray-900">
+              Toutes les lignes
+            </div>
+            <div class="grid grid-cols-4 gap-y-4 gap-x-8">
+              <NuxtLink v-for="voie in voies" :key="voie.line" :href="voie._path" @click="close()" class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50">
+                <div class="flex-shrink-0">
+                  <div
+                    class="h-10 w-10 rounded-full flex items-center justify-center text-white font-bold"
+                    :style="`background-color: ${voie.color}`"
+                  >
+                  {{ voie.line }}
+                  </div>
+                </div>
               </NuxtLink>
             </div>
           </div>
@@ -111,4 +142,8 @@ const navItems = [
   { name: 'Plan officel', path: '/plan-officiel' },
   { name: 'Carte interactive', path: '/carte-interactive' },
 ]
+
+const { data: voies } = await useAsyncData(() => {
+  return queryContent('/').where({ _type: 'json' }).find()
+})
 </script>
