7a9357262464ae3acf2e856308506fa5d3ae7874
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-09-25 11:15:16 +0200

    add stat section on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <HomeHeroSection/>
+    <HomeStatSection />
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8 lg:py-24">
       <div class="space-y-8 sm:space-y-12">
         <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl text-center">
           <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Avancement par ligne</h2>
-          <p class="text-xl text-gray-500">Choisissez l'une des 13 voies lyonnaises pour voir son niveau d'avancement.</p>
+          <p class="text-xl text-gray-500">Choisissez l'une des 12 voies lyonnaises pour voir son niveau d'avancement.</p>
         </div>
         <HomeLinesSection class="mt-5"/>
       </div>
