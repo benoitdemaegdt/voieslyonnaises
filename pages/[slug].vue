1e5a0ea9eaa3157e6f1c975445b8968eb82a4366
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-09-25 10:45:13 +0200

    fix wording

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -22,7 +22,7 @@
         </div>
       </div>
 
-      <!-- Product info -->
+      <!-- info -->
       <div class="max-w-2xl mx-auto pt-10 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
         <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
           <h1 class="flex items-center text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
