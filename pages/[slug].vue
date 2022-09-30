3276d469d084f01da3eafd69b987a4ced4463bcc
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-09-30 19:20:34 +0200

    reorder grid for better mobile page

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -22,8 +22,10 @@
         </div>
       </div>
 
-      <!-- info -->
+
       <div class="max-w-2xl mx-auto pt-10 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
+
+        <!-- Titre (Nom de la ligne) -->
         <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
           <h1 class="flex items-center text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
             <div>Ligne</div>
@@ -36,44 +38,8 @@
           </h1>
         </div>
 
-        <!-- Dernières nouvelles -->
-        <div class="mt-4 lg:mt-0 lg:row-span-3">
-          <h2 class="text-xl font-bold sm:text-2xl text-gray-900">Dernières nouvelles</h2>
-          <div class="mt-6">
-            <section aria-labelledby="announcements-title" class="flex flex-col gap-16">
-              <div class="overflow-hidden">
-                  <div class="flow-root">
-                    <ul role="list" class="-my-5">
-                      <li v-for="(announcement, index) in voie.announcements" :key="index" class="py-5">
-                        <div class="relative">
-                          <time class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 pl-3.5" datetime="2022-09-05">
-                            <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
-                              <span class="h-4 w-0.5 bg-zinc-200"></span>
-                            </span>
-                            {{ announcement.date}}
-                          </time>
-                          <h3 class="text-sm font-semibold text-gray-800">{{ announcement.title }}</h3>
-                          <p class="mt-1 text-sm text-gray-600 line-clamp-2">{{ announcement.text }}</p>
-                          <NuxtLink v-if="announcement.link" :to="announcement.link" target="_blank" area-hidden="true" class="relative z-10 mt-2 flex items-center text-sm font-medium" :style="`color: ${voie.color}`">
-                            {{ announcement.anchor }}
-                            <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
-                              <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                            </svg>
-                          </NuxtLink>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                  <div v-if="voie.announcements.length > 4" class="mt-6">
-                    <button class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Tout voir </button>
-                  </div>
-              </div>
-            </section>
-          </div>
-        </div>
-
+        <!-- Description, stats & map -->
         <div class="py-10 lg:py-6 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-          <!-- Description and stats -->
           <div>
             <h3 class="sr-only">Description</h3>
             <div class="space-y-6">
@@ -106,10 +72,45 @@
               </dl>
             </div>
           </div>
-
           <section aria-labelledby="shipping-heading" class="mt-10">
             <Map :voie="voie"/>
           </section>
+        </div>
+
+        <!-- Dernières nouvelles -->
+        <div class="mt-4 lg:mt-0 lg:row-start-1 lg:col-start-3 lg:row-span-3">
+          <h2 class="text-xl font-bold sm:text-2xl text-gray-900">Dernières nouvelles</h2>
+          <div class="mt-6">
+            <section aria-labelledby="announcements-title" class="flex flex-col gap-16">
+              <div class="overflow-hidden">
+                  <div class="flow-root">
+                    <ul role="list" class="-my-5">
+                      <li v-for="(announcement, index) in voie.announcements" :key="index" class="py-5">
+                        <div class="relative">
+                          <time class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 pl-3.5" datetime="2022-09-05">
+                            <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
+                              <span class="h-4 w-0.5 bg-zinc-200"></span>
+                            </span>
+                            {{ announcement.date}}
+                          </time>
+                          <h3 class="text-sm font-semibold text-gray-800">{{ announcement.title }}</h3>
+                          <p class="mt-1 text-sm text-gray-600 line-clamp-2">{{ announcement.text }}</p>
+                          <NuxtLink v-if="announcement.link" :to="announcement.link" target="_blank" area-hidden="true" class="relative z-10 mt-2 flex items-center text-sm font-medium" :style="`color: ${voie.color}`">
+                            {{ announcement.anchor }}
+                            <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
+                              <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                          </NuxtLink>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                  <div v-if="voie.announcements.length > 4" class="mt-6">
+                    <button class="w-full flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Tout voir </button>
+                  </div>
+              </div>
+            </section>
+          </div>
         </div>
 
       </div>
