73068ba9f6a0eb254af13e67d6dec8d5d0c852b2
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-10-17 21:32:40 +0200

    add meta informations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,38 @@
-import { defineNuxtConfig } from 'nuxt'
+const TITLE = 'Voies Lyonnaises'
+const DESCRIPTION = 'Avancement des voies lyonnaises'
+const BASE_URL = 'https://voieslyonnaises.netlify.app'
+const COVER_IMAGE_URL = 'https://res.cloudinary.com/monpetitsommet/image/upload/v1663494829/voieslyonnaises/voies-lyonnaises_wkew1x.png'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   meta: {
     htmlAttrs: { lang: 'fr' },
-    title: 'Voies Lyonnaises',
+    title: TITLE,
     meta: [
-      { hid: 'description', name: 'description', content: 'avancement des voies lyonnaises' },
+      { hid: 'description', name: 'description', content: DESCRIPTION },
+      // facebook
+      { property: 'og:site_name', content: TITLE },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: BASE_URL },
+      { hid: 'og:title', property: 'og:title', content: TITLE },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: DESCRIPTION,
+      },
+      { hid: 'og:image', property: 'og:image', content: COVER_IMAGE_URL },
+      { property: 'og:image:width', content: '640' },
+      { property: 'og:image:height', content: '476' },
+      // twitter
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:url', name: 'twitter:url', content: BASE_URL },
+      { hid: 'twitter:title', name: 'twitter:title', content: TITLE },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: DESCRIPTION,
+      },
+      { hid: 'twitter:image', name: 'twitter:image', content: COVER_IMAGE_URL },
     ]
-  },
-  runtimeConfig: {
-    public: {
-      mapboxAccessToken: process.env.NUXT_MAPBOX_ACCESS_TOKEN,
-    },
   },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'nuxt-icon'],
   tailwindcss: { viewer: false },
