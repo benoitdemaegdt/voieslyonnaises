07f99884a8ba9534065d42af9c627c0ae8d27989
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-11-06 11:10:30 +0100

    add static urls to sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,15 +1,22 @@
 import { serverQueryContent } from '#content/server'
 import { SitemapStream, streamToPromise } from 'sitemap'
+
+const BASE_URL = 'https://voieslyonnaises.netlify.app'
+const STATIC_ENDPOINTS = [
+  '/',
+  '/carte-interactive',
+  '/plan-officiel',
+  '/contribuer',
+]
+
 export default defineEventHandler(async (event) => {
   const docs = await serverQueryContent(event).find()
-  const sitemap = new SitemapStream({
-    hostname: 'https://voieslyonnaises.netlify.app'
-  })
+  const sitemap = new SitemapStream({ hostname: BASE_URL })
   for (const doc of docs) {
-    sitemap.write({
-      url: doc._path,
-      changefreq: 'monthly'
-    })
+    sitemap.write({ url: doc._path, changefreq: 'monthly' })
+  }
+  for (const staticEndpoint of STATIC_ENDPOINTS) {
+    sitemap.write({ url: staticEndpoint, changefreq: 'monthly' })
   }
   sitemap.end()
   return streamToPromise(sitemap)
