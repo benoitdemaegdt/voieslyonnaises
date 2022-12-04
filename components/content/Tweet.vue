51823fd9149694f81aa37f6c464caacd549d7aa1
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-12-04 11:16:33 +0100

    refacto tweet embed with custom component

--- components/content/Tweet.vue
+++ components/content/Tweet.vue
@@ -1,10 +1,50 @@
 <template>
-  <Tweet :tweet-id="tweetId" />
+  <div ref="tweetContainer" v-bind="{tweetId}"></div>
 </template>
 
 <script setup>
-import Tweet from "vue-tweet";
-defineProps({
-  tweetId: { type: String, required: true },
-})
+const { tweetId } = defineProps({ tweetId: { type: String, required: true }, })
+const tweetContainer = ref()
+
+onMounted(() => renderTweet());
+
+// https://github.com/DannyFeliz/vue-tweet/blob/main/src/components/vue-tweet.vue
+function renderTweet() {
+  if (!(window["twttr"] && window["twttr"].ready)) {
+    addScript("https://platform.twitter.com/widgets.js", renderTweet);
+    return;
+  }
+  window["twttr"]
+    .ready()
+    .then(({ widgets }) => {
+      if (tweetContainer.value) {
+        tweetContainer.value.innerHTML = "";
+      }
+      const { tweetId } = getTweetParams();
+      widgets
+        .createTweet(tweetId, tweetContainer.value)
+        .then(async () => {
+          await nextTick();
+        })
+      });
+    }
+
+function getTweetParams() {
+  console.log('twwetId >>', tweetId)
+  if (tweetId) {
+    if (!/^\d+$/.test(tweetId)) {
+      throw new Error('Invalid tweet-id')
+    }
+  } else {
+    throw new Error('Must provide tweet-id')
+  }
+  return { tweetId }
+}
+
+function addScript(src, cb) {
+  const s = document.createElement("script");
+  s.setAttribute("src", src);
+  s.addEventListener("load", () => cb(), false);
+  document.body.appendChild(s);
+}
 </script>
