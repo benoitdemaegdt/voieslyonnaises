91b58b9a5008d1f3dffb8de29ab38741e18fdeff
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2022-12-27 08:46:11 +0100

    make design consistent

--- pages/blog/chronologie/index.vue
+++ pages/blog/chronologie/index.vue
@@ -10,8 +10,15 @@
     <li class="mb-10 ml-4" v-for="article in articles">
         <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-2 -left-1.5 border border-white"></div>
         <h3 class="text-lg font-semibold text-gray-800 capitalize">{{ getDateText(article.date) }}</h3>
-        <p class="mb-4 mt-1 text-base font-normal text-gray-500 ">{{ article.description }}</p>
-        <NuxtLink :to="article._path" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:border-gray-300 hover:text-official focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-official">Détails <svg class="ml-2 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></NuxtLink>
+        <div class="rounded-lg hover:bg-gray-50 p-2 mt-2">
+          <NuxtLink :to="article._path">
+            <h4>{{ article.description }}</h4>
+            <div class="text-official">
+              <span class="text-base">détails</span>
+              <Icon name="mdi:chevron-right" class="h-5 w-5" aria-hidden="true"></Icon>
+            </div>
+          </NuxtLink>
+        </div>
     </li>
   </ol>
 </div>
