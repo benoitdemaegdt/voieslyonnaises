432fa669cdabc4998c98a96b43d7800b5bfb7509
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-05-24 08:51:28 +0200

    plan officiel - better quality

--- pages/plan-officiel/index.vue
+++ pages/plan-officiel/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full">
     <img
-      src="~/assets/plan-officiel.jpeg"
-      class="object-contain w-full h-full"
+      src="~/assets/plan-officiel.png"
+      class="object-contain w-full h-screen"
       alt="plan officiel des voies lyonnaises"
-    />
+    >
   </div>
 </template>
