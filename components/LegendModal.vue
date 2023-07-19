8952af29492fe99fd8a3ca5ca2fc5bfd5cc87c03
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-07-19 23:32:39 +0200

    move legend in vue component

--- components/LegendModal.vue
+++ components/LegendModal.vue
@@ -1,92 +1,103 @@
 <template>
-  <TransitionRoot appear :show="isOpen" as="template">
-    <Dialog as="div" class="relative z-10" @close="closeModal">
-      <TransitionChild
-        as="template"
-        enter="duration-300 ease-out"
-        enter-from="opacity-0"
-        enter-to="opacity-100"
-        leave="duration-200 ease-in"
-        leave-from="opacity-100"
-        leave-to="opacity-0"
-      >
-        <div class="fixed inset-0 bg-black bg-opacity-25" />
-      </TransitionChild>
+  <Dialog :open="isOpen" class="relative z-50" @close="closeModal">
+    <!-- backdrop-->
+    <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
 
-      <div class="fixed inset-0 overflow-y-auto">
-        <div
-          class="flex min-h-full items-center justify-center p-4 text-center"
+    <!-- dialog itself-->
+    <div class="fixed inset-0 flex items-center justify-center p-4">
+      <DialogPanel class="relative p-4 w-full max-w-sm rounded bg-white">
+        <button
+          type="button"
+          class="absolute top-1 right-1 bg-white rounded-md p-1 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100"
+          @click="closeModal"
         >
-          <TransitionChild
-            as="template"
-            enter="duration-300 ease-out"
-            enter-from="opacity-0 scale-95"
-            enter-to="opacity-100 scale-100"
-            leave="duration-200 ease-in"
-            leave-from="opacity-100 scale-100"
-            leave-to="opacity-0 scale-95"
-          >
-            <DialogPanel
-              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
-            >
-              <DialogTitle
-                as="h3"
-                class="text-lg font-medium leading-6 text-gray-900"
-              >
-                Payment successful
-              </DialogTitle>
-              <div class="mt-2">
-                <p class="text-sm text-gray-500">
-                  Your payment has been successfully submitted. We’ve sent you
-                  an email with all of the details of your order.
-                </p>
+          <Icon name="mdi:close" class="h-6 w-6" aria-hidden="true" />
+        </button>
+        <DialogTitle class="text-lg font-medium leading-6 text-gray-900">
+          Légende
+        </DialogTitle>
+        <div class="mt-2">
+          <div class="grid grid-cols-[64px_1fr] gap-x-4">
+            <div class="my-auto">
+              <div class="h-1 relative">
+                <div class="absolute h-full w-full">
+                  <div class="h-full bg-lvv-blue-600 dashed-line" />
+                </div>
               </div>
+            </div>
+            <div>prévu pour 2026</div>
 
-              <div class="mt-4">
-                <button
-                  type="button"
-                  class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                  @click="closeModal"
-                >
-                  Got it, thanks!
-                </button>
+            <div class="my-auto">
+              <div class="h-1 bg-lvv-blue-600" />
+            </div>
+            <div>terminé</div>
+
+            <div class="my-auto">
+              <div class="h-1 relative">
+                <div class="absolute h-full w-full">
+                  <div class="h-full bg-lvv-blue-600 dashed-line animated-dashes" />
+                </div>
               </div>
-            </DialogPanel>
-          </TransitionChild>
+            </div>
+            <div>en travaux</div>
+
+            <div class="my-auto">
+              <div class="h-2 bg-lvv-blue-600 opacity-30" />
+            </div>
+            <div>linéaire inconnu</div>
+
+            <div class="my-auto">
+              <div class="text-lvv-blue-600 font-bold">
+                x x x x x
+              </div>
+            </div>
+            <div>abandonné pour 2026</div>
+          </div>
         </div>
-      </div>
-    </Dialog>
-  </TransitionRoot>
+      </DialogPanel>
+    </div>
+  </Dialog>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import {
-  TransitionRoot,
-  TransitionChild,
   Dialog,
   DialogPanel,
   DialogTitle
 } from '@headlessui/vue'
 
-const props = defineProps({
-  isLegendModalOpen: {
-    type: Boolean,
-    default: () => false
-  }
-})
+const isOpen = ref(false)
 
-const isOpen = ref(props.isLegendModalOpen)
-
-console.log(props.isLegendModalOpen)
-// const isOpen = ref(true)
-//
 function closeModal () {
   isOpen.value = false
 }
-//
-// // called from Map.vue
-// function openModal () {
-//   isOpen.value = true
-// }
+function openModal () {
+  isOpen.value = true
+}
+
+defineExpose({
+  openModal
+})
 </script>
+
+<style>
+.dashed-line {
+    background-image: linear-gradient(to right, transparent 50%, white 50%);
+    background-position: 0 0;
+    background-repeat: repeat-x;
+    background-size: 12px 0.25rem;
+}
+
+.animated-dashes {
+    animation: dash-animation 0.5s linear infinite;
+}
+
+@keyframes dash-animation {
+    0% {
+        background-position: 0 0;
+    }
+    100% {
+        background-position: 12px 0;
+    }
+}
+</style>
--- components/Map.vue
+++ components/Map.vue
@@ -1,63 +1,6 @@
 <template>
   <div>
-    <Dialog :open="isOpen" class="relative z-50" @close="closeModal">
-      <!-- backdrop-->
-      <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
-
-      <!-- dialog itself-->
-      <div class="fixed inset-0 flex items-center justify-center p-4">
-        <DialogPanel class="relative p-4 w-full max-w-sm rounded bg-white">
-          <button
-            type="button"
-            class="absolute top-1 right-1 bg-white rounded-md p-1 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100"
-            @click="closeModal"
-          >
-            <Icon name="mdi:close" class="h-6 w-6" aria-hidden="true" />
-          </button>
-          <DialogTitle class="text-lg font-medium leading-6 text-gray-900">
-            Légende
-          </DialogTitle>
-          <div class="mt-2">
-            <div class="grid grid-cols-[64px_1fr] gap-x-4">
-              <div class="my-auto">
-                <div class="h-1 relative">
-                  <div class="absolute h-full w-full">
-                    <div class="h-full bg-lvv-blue-600 dashed-line" />
-                  </div>
-                </div>
-              </div>
-              <div>prévu pour 2026</div>
-
-              <div class="my-auto">
-                <div class="h-1 bg-lvv-blue-600" />
-              </div>
-              <div>terminé</div>
-
-              <div class="my-auto">
-                <div class="h-1 relative">
-                  <div class="absolute h-full w-full">
-                    <div class="h-full bg-lvv-blue-600 dashed-line animated-dashes" />
-                  </div>
-                </div>
-              </div>
-              <div>en travaux</div>
-
-              <div class="my-auto">
-                <div class="h-2 bg-lvv-blue-600 opacity-30" />
-              </div>
-              <div>linéaire inconnu</div>
-
-              <div class="my-auto">
-                <div class="text-lvv-blue-600 font-bold">
-                  x x x x x
-                </div>
-              </div>
-              <div>abandonné pour 2026</div>
-            </div>
-          </div>
-        </DialogPanel>
-      </div>
-    </Dialog>
+    <LegendModal ref="legendModalComponent" />
     <div id="map" class="rounded-lg" style="height: 40vh" />
   </div>
 </template>
@@ -65,8 +8,6 @@
 <script setup>
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
-import { Dialog, DialogTitle, DialogPanel } from '@headlessui/vue'
-import { ref } from 'vue'
 import style from '@/assets/style.json'
 import LegendControl from '@/maplibre/LegendControl'
 
@@ -74,15 +15,9 @@ const { geojson } = defineProps({
   geojson: { type: Object, required: true }
 })
 
-const { plotDoneSections, plotWipSections, plotPlannedSections, plotVarianteSections, plotUnknownSections, plotAbandonedSections, fitBounds } = useMap()
+const legendModalComponent = ref(null)
 
-const isOpen = ref(false)
-function closeModal () {
-  isOpen.value = false
-}
-function openModal () {
-  isOpen.value = true
-}
+const { plotDoneSections, plotWipSections, plotPlannedSections, plotVarianteSections, plotUnknownSections, plotAbandonedSections, fitBounds } = useMap()
 
 onMounted(() => {
   const map = new maplibregl.Map({
@@ -96,7 +31,7 @@ onMounted(() => {
   map.addControl(new maplibregl.FullscreenControl(), 'top-right')
   map.addControl(new maplibregl.AttributionControl({ compact: true }), 'bottom-left')
   const legendControl = new LegendControl({
-    onClick: () => openModal()
+    onClick: () => legendModalComponent.value.openModal()
   })
   map.addControl(legendControl, 'top-right')
 
@@ -125,25 +60,4 @@ onMounted(() => {
     background-image: url('~/maplibre/info.svg');
     background-size: 85%;
 }
-
-.dashed-line {
-    background-image: linear-gradient(to right, transparent 50%, white 50%);
-    background-position: 0 0;
-    background-repeat: repeat-x;
-    background-size: 12px 0.25rem;
-}
-
-.animated-dashes {
-    animation: dash-animation 0.5s linear infinite;
-}
-
-@keyframes dash-animation {
-    0% {
-        background-position: 0 0;
-    }
-    100% {
-        background-position: 12px 0;
-    }
-}
-
 </style>
