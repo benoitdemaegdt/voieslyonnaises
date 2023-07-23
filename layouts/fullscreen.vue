958d6f7c29250c3ce67eec7f9092684849443cce
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-07-23 23:08:30 +0200

    try fixing h-screen issue on ios

--- layouts/fullscreen.vue
+++ layouts/fullscreen.vue
@@ -4,3 +4,12 @@
     <slot class="flex flex-grow" />
   </div>
 </template>
+
+<style>
+@supports (-webkit-touch-callout: none) {
+    .h-screen {
+        height: 100vh;
+        max-height: -webkit-fill-available !important;
+    }
+}
+</style>
