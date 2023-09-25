f7d0f6f3e502ddc612cb716d0fd5ec99f321ea83
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-09-25 09:13:16 +0200

    fix nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 const TITLE = 'Cyclopolis - Suivi des Voies Lyonnaises par La Ville à Vélo';
 const DESCRIPTION =
-  'Plateforme citoyenne et associative, par La Ville à Vélo. Etat d'avancement, cartes interactives des itinéraires, détails, travaux : suivez le développement du réseau cyclable sécurisé lyonnais';
+  "Plateforme citoyenne et associative, par La Ville à Vélo. Etat d'avancement, cartes interactives des itinéraires, détails, travaux : suivez le développement du réseau cyclable sécurisé lyonnais";
 const BASE_URL = 'https://cyclopolis.fr';
 const COVER_IMAGE_URL = 'https://cyclopolis.lavilleavelo.org/cyclopolis.png';
 
