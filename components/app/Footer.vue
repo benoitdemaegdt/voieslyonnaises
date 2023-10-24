2d3b3d8438206280622f5791e8d6cc67c831a92f
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-10-24 07:47:06 +0200

    Add link to changelog in footer

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -41,7 +41,8 @@
 <script setup>
 
 const links = [
-  { name: 'Mentions légales', path: '/mentions-legales' }
+  { name: 'Mentions légales', path: '/mentions-legales' },
+  { name: 'Historique', path: '/changelog' }
 ]
 
 const socials = {
