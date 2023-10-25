4ed8bb7d2f99e9d71abd74696cf1ff7fd69a1842
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-10-25 22:52:47 +0200

    add embedable map

---
+++ layouts/embed.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <slot class="flex flex-grow" />
+  </div>
+</template>
+
+<style>
+@supports (-webkit-touch-callout: none) {
+    .h-screen {
+        height: 100vh;
+        max-height: -webkit-fill-available !important;
+    }
+}
+</style>
