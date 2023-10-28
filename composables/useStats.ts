193fa7720248fe33b39b480ce326e0bdd373dbf8
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-10-28 22:33:01 +0200

    now compute distance in stats

--- composables/useStats.ts
+++ composables/useStats.ts
@@ -2,15 +2,15 @@ type Feature = {
   type: string;
   properties: {
     id?: string;
-    line?: string;
-    color?: string;
-    name?: string;
-    distance?: number;
-    status?: string;
+    line: string;
+    color: string;
+    name: string;
+    distance: number;
+    status: string;
   };
   geometry: {
     type: string;
-    coordinates: number[] | number[][];
+    coordinates: number[][];
   };
 };
 
@@ -37,18 +37,52 @@ export const useStats = () => {
       });
   }
 
-  function getDistance({ allSections, status }) {
+  function getDistance({ allSections, status }: { allSections: Feature[]; status: string[] }) {
     const distanceInMeters = allSections
-      .filter(feature => status.includes(feature.properties.status))
-      .reduce((acc, section) => {
-        if (!section.properties.distance) {
-          console.log('section >>', section);
-          return acc;
-        }
-        return acc + section.properties.distance;
+      .filter((feature: Feature) => status.includes(feature.properties.status))
+      .reduce((acc: number, feature: Feature) => {
+        return acc + getLineStringDistance(feature);
       }, 0);
 
     return Math.round(distanceInMeters / 1000);
+  }
+
+  function getLineStringDistance(feature: Feature) {
+    if (feature.geometry.type !== 'LineString') {
+      throw new Error('[getLineStringDistance] Feature must be a LineString');
+    }
+
+    let distance = 0;
+    const coordinates = feature.geometry.coordinates;
+
+    for (let i = 0; i < coordinates.length - 1; i++) {
+      const [lon1, lat1] = coordinates[i];
+      const [lon2, lat2] = coordinates[i + 1];
+      distance += haversine(lat1, lon1, lat2, lon2);
+    }
+
+    return distance;
+  }
+
+  function haversine(lat1: number, lon1: number, lat2: number, lon2: number) {
+    // Convert latitude and longitude from degrees to radians
+    const toRadians = (angle: number) => (angle * Math.PI) / 180;
+    lat1 = toRadians(lat1);
+    lon1 = toRadians(lon1);
+    lat2 = toRadians(lat2);
+    lon2 = toRadians(lon2);
+
+    // Haversine formula
+    const dLat = lat2 - lat1;
+    const dLon = lon2 - lon1;
+    const a = Math.sin(dLat / 2) ** 2 + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) ** 2;
+    const c = 2 * Math.asin(Math.sqrt(a));
+
+    // Radius of the Earth in meters
+    const radius = 6371000;
+
+    // Calculate the distance in meters
+    return Math.round(radius * c);
   }
 
   return { getAllUniqSections, getDistance };
