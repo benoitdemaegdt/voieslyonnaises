ebbc504036b41f35e951a47f51d6ba80e085843d
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-10-31 08:36:13 +0100

    maj historique

--- pages/historique.vue
+++ pages/historique.vue
@@ -5,28 +5,40 @@
         <h1 class="text-4xl font-extrabold tracking-tight text-slate-900 sm:text-5xl">
           Historique des changements
         </h1>
-        <p class="mt-4 text-base leading-7 text-slate-600">
+        <div class="mx-auto mt-4 text-gray-500 prose prose-lg text-center">
           Suivez tous les changements qui sont réalisés sur la plateforme.
-        </p>
+        </div>
       </div>
     </div>
     <div class="relative mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 mb-8">
       <section class="md:flex">
         <h2 class="pl-7 text-sm leading-6 text-slate-500 md:w-1/4 md:pl-0 md:pr-12 md:text-right">
-          23 octobre 2023
+          16 octobre 2023
         </h2>
         <div class="relative pl-7 pt-2 md:w-3/4 md:pl-12 md:pt-0 pb-16">
           <div class="absolute bottom-0 left-0 w-px bg-slate-200 -top-3 md:top-2.5" />
           <div class="absolute -left-1 -top-[1.0625rem] h-[0.5625rem] w-[0.5625rem] rounded-full border-2 border-slate-300 bg-white md:top-[0.4375rem]" />
-          <div class="max-w-none prose-h3:mb-4 prose">
-            <h3>Ajustements sur la Voie Lyonnaise 2</h3>
-            <p>Le tronçon qui passe sous le périphérique (boulevard Laurent Bonnevay) et qui permet de relier le 8ème arrondissement de Lyon à Vénissieux ne sera pas modifié. Il est donc désormais considéré comme terminé. <a href="https://met.grandlyon.com/une-voie-verte-pour-aller-a-grand-parilly/" target="_blank">Ce tronçon a été créé en 2019</a>. </p>
-            <NuxtLink to="/voie-lyonnaise-2">
-              Voir le détail de la voie lyonnaise n°2
+          <div class="max-w-none prose-h3:mb-4 prose prose-lg text-gray-500">
+            <h3>Décision de la Métropole sur la Voie Lyonnaise n°8 !</h3>
+            <p>Dans la concertation de mars-avril 2023 sur le tronçon Tassin - Lyon 5 de la Voie Lyonnaise n°8, la Métropole proposait 2 variantes pour relier l'Avenue de la République et l'Avenue Victor Hugo&nbsp;:</p>
+            <ul>
+              <li>Une variante par l'avenue Vincent Serre</li>
+              <li>Une variante par la rue de la Pépinière</li>
+            </ul>
+            <p>Lors de sa commission permanente du 16 octobre 2023, la Métropole a finalement validé la première variante, par l'avenue Vincent Serre.</p>
+            <p>
+              <a href="https://jeparticipe.grandlyon.com/media/default/0001/01/bd50d56d86221d7c4daf56f310abe3cb77aff211.pdf" target="_blank">Voir le dossier de concertation (p18)</a>
+            </p>
+            <p>
+              <a href="https://agora.grandlyon.com/portail/jsp/openfile.jsp?pdf=A9iCZwGvgK5FubNtu322bcS53GS0sK4fqUNzpPiImFcNNkx%2BTqSn6NcCoEvAfojpQXos53usMrkPJoSRf%2FIaOpbByk853Y1HvC8f5zVB7%2BI2fdWBM0KS7G%2Bc3TocC2uS" target="_blank">Voir le rapport de la commission</a>
+            </p>
+            <NuxtLink to="/voie-lyonnaise-8">
+              Voir le détail de la voie lyonnaise n°8
             </NuxtLink>
           </div>
         </div>
       </section>
+
       <section class="md:flex">
         <h2 class="pl-7 text-sm leading-6 text-slate-500 md:w-1/4 md:pl-0 md:pr-12 md:text-right">
           2 octobre 2023
@@ -34,7 +46,7 @@
         <div class="relative pl-7 pt-2 md:w-3/4 md:pl-12 md:pt-0 pb-16">
           <div class="absolute bottom-0 left-0 w-px bg-slate-200 -top-3 md:top-2.5" />
           <div class="absolute -left-1 -top-[1.0625rem] h-[0.5625rem] w-[0.5625rem] rounded-full border-2 border-slate-300 bg-white md:top-[0.4375rem]" />
-          <div class="max-w-none prose-h3:mb-4 prose">
+          <div class="max-w-none prose-h3:mb-4 prose prose-lg text-gray-500">
             <h3>Lancement officiel de Cyclopolis</h3>
             <p>Après plusieurs mois de travail, l'association "La&nbsp;Ville&nbsp;à&nbsp;Vélo" lance sa plateforme de suivi de l'avancement des Voies Lyonnaises.</p>
             <a href="https://lavilleavelo.org/2023/10/02/la-ville-a-velo-lance-cyclopolis-la-plateforme-pour-suivre-de-pres-lavancement-des-voies-lyonnaises/" target="_blank">Lire le communiqué officiel</a>
@@ -44,3 +56,5 @@
     </div>
   </div>
 </template>
+<script setup lang='ts'>
+</script>
