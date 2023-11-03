4ff75af932a75be25cdce4fb1ae727f03519517a
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-11-03 09:37:28 +0100

    wip - add news banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
+    <NewsBanner v-if="!isNewsBannerClosed" />
     <AppHeader class="sticky top-0" />
     <slot />
     <AppFooter />
   </div>
 </template>
+
+<script setup>
+const isNewsBannerClosed = localStorage.getItem('isNewsBannerClosed') === 'true';
+</script>
