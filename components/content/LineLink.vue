fb7f0998653b992f0418981ad5734a265d98383f
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-11-06 22:36:10 +0100

    few changes

--- components/content/LineLink.vue
+++ components/content/LineLink.vue
@@ -1,21 +1,21 @@
 <template>
-  <a :href="`/voie-lyonnaise-${voie.line}`" :style="`color: ${voie.color}; text-decoration-color: ${voie.color};`">
+  <NuxtLink :to="`/voie-lyonnaise-${line}`" :style="`color: ${color}; text-decoration-color: ${color};`">
     Voie Lyonnaise
     <span
       class="h-6 w-6 rounded-full inline-flex items-center justify-center text-white"
-      :style="`background-color: ${voie.color};`"
+      :style="`background-color: ${color};`"
     >
-      {{ voie.line }}
+      {{ line }}
     </span>
-  </a>
+  </NuxtLink>
 </template>
 
 <script setup>
+const { getLineColor } = useColors();
+
 const props = defineProps({
   line: { type: String, required: true }
-})
+});
 
-const { data: voie } = await useAsyncData(props.line, () => {
-  return queryContent('voies-lyonnaises').where({ _type: 'markdown', line: Number(props.line) }).findOne()
-})
+const color = getLineColor(Number(props.line));
 </script>
