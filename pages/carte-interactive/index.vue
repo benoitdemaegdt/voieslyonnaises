3f1a92c1817d774ef2bc6c6ef3f28fe551874c27
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-11-06 21:52:55 +0100

    fix eslint

--- pages/carte-interactive/index.vue
+++ pages/carte-interactive/index.vue
@@ -9,16 +9,16 @@
 definePageMeta({
   pageTransition: false,
   layout: 'fullscreen'
-})
+});
 
 const { data: voies } = await useAsyncData(() => {
-  return queryContent('voies-lyonnaises').where({ _type: 'json' }).find()
-})
+  return queryContent('voies-lyonnaises').where({ _type: 'json' }).find();
+});
 
-const features = voies.value.map(voie => voie.features).flat()
+const features = voies.value.map(voie => voie.features).flat();
 
-const description = 'Découvrez la carte interactive des Voies Lyonnaises. Itinéraires rue par rue. Plan régulièrement mis à jour pour une information complète.'
-const COVER_IMAGE_URL = 'https://cyclopolis.lavilleavelo.org/cyclopolis.png'
+const description = 'Découvrez la carte interactive des Voies Lyonnaises. Itinéraires rue par rue. Plan régulièrement mis à jour pour une information complète.';
+const COVER_IMAGE_URL = 'https://cyclopolis.lavilleavelo.org/cyclopolis.png';
 useHead({
   title: 'Carte à jour des Voies Lyonnaises',
   meta: [
@@ -30,5 +30,5 @@ useHead({
     { hid: 'og:image', property: 'og:image', content: COVER_IMAGE_URL },
     { hid: 'twitter:image', name: 'twitter:image', content: COVER_IMAGE_URL }
   ]
-})
+});
 </script>
