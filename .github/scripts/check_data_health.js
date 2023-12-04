20dec7f60bd24e4a24b6f49b088cf00bb940e4e6
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-12-04 21:29:03 +0100

    remove useless console.log

--- .github/scripts/check_data_health.js
+++ .github/scripts/check_data_health.js
@@ -16,7 +16,6 @@ function checkJsonFilesAreValid(directory = 'content') {
     } else if (file.endsWith('.json')) {
       try {
         JSON.parse(fs.readFileSync(filePath));
-        console.log(`JSON file is valid: ${filePath}`);
       } catch (error) {
         console.error(`Invalid JSON file: ${filePath}`);
         process.exit(1);
