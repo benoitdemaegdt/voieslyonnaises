24c2cee8cac6027ed922bf2165af568c29063bd6
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-01-16 21:52:44 +0100

    try fix h-screen

--- layouts/fullscreen.vue
+++ layouts/fullscreen.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="flex flex-col h-[calc(100dvh)]">
+  <div class="flex flex-col h-screen">
     <AppHeader />
     <slot class="flex flex-grow" />
   </div>
 </template>
+
+<style scoped>
+.h-screen {
+  height: 100vh; /* Fallback for browsers that do not support Custom Properties */
+  height: calc(100dvh);
+}
+</style>
