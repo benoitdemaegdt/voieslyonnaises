1c66e1d4979383eb2faa0f7225fe5852ea30afbc
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-01-16 21:32:26 +0100

    try fix h-screen

--- layouts/fullscreen.vue
+++ layouts/fullscreen.vue
@@ -4,12 +4,3 @@
     <slot class="flex flex-grow" />
   </div>
 </template>
-
-<style>
-@supports (-webkit-touch-callout: none) {
-    .h-screen {
-        height: 100vh;
-        max-height: -webkit-fill-available !important;
-    }
-}
-</style>
