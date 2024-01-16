9d2dd27b771b127da318ef10fc33e757507ecde9
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-01-16 21:59:27 +0100

    try fix h-screen

--- layouts/fullscreen.vue
+++ layouts/fullscreen.vue
@@ -5,9 +5,16 @@
   </div>
 </template>
 
+<script setup>
+onBeforeMount(() => {
+  document.documentElement.style.setProperty('--vh', window.innerHeight * 0.01 + 'px');
+});
+
+</script>
+
 <style scoped>
 .h-screen {
   height: 100vh; /* Fallback for browsers that do not support Custom Properties */
-  height: calc(100dvh);
+  height: calc(var(--vh, 1vh) * 100);
 }
 </style>
