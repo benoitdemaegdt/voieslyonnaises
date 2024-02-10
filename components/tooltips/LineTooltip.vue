0b75ed6207a3b3066489abd1ef21ac79a9eaffc7
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-02-10 21:58:47 +0100

    design

--- components/tooltips/LineTooltip.vue
+++ components/tooltips/LineTooltip.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="not-prose w-48">
-    <div class="pb-1 bg-zinc-100 flex flex-col items-center justify-center">
+    <div class="py-1 bg-zinc-100 flex flex-col items-center justify-center">
       <div class="text-gray-900 font-bold text-lg">
         Voie Lyonnaise
       </div>
@@ -11,28 +11,34 @@
         {{ feature.properties.line }}
       </div>
     </div>
-    <div class="p-2 divide-y">
-      <div>
-        <div class="text-sm font-bold">
+    <div class="px-2 divide-y">
+      <div class="py-1 flex flex-col items-center">
+        <div class="text-base font-bold">
           Tronçon
         </div>
-        <div>{{ feature.properties.name }}</div>
+        <div class="text-sm text-center">
+          {{ feature.properties.name }}
+        </div>
       </div>
-      <div>
-        <div class="text-sm font-bold">
+      <div class="py-1 flex items-center justify-between">
+        <div class="text-base font-bold">
           statut
         </div>
-        <div>{{ getStatusText(feature.properties.status, feature.properties.doneAt) }}</div>
+        <div class="text-sm">
+          {{ getStatusText(feature.properties.status) }}
+        </div>
       </div>
-      <div>
-        <div class="text-sm font-bold">
+      <div class="py-1 flex items-center justify-between">
+        <div class="text-base font-bold">
           Longueur
         </div>
-        <div>{{ Math.round(getDistance({ features: [feature] }) / 25) * 25 }}m</div>
+        <div class="text-sm">
+          {{ Math.round(getDistance({ features: [feature] }) / 25) * 25 }}m
+        </div>
       </div>
     </div>
     <div class="bg-lvv-blue-600 flex justify-center">
-      <a class="p-1 text-white text-sm italic hover:underline" :href="`/voie-lyonnaise-${feature.properties.line}`" target="_blank">
+      <a class="p-1 text-white text-base italic hover:underline" :href="`/voie-lyonnaise-${feature.properties.line}`" target="_blank">
         voir le détail <Icon name="mdi:link-variant" class="h-4 w-4 text-white" />
       </a>
     </div>
@@ -61,26 +67,26 @@ const { feature } = defineProps<{
   }
 }>();
 
-function getDoneAtText(doneAt: string): string {
-  const [day, month, year] = doneAt.split('/');
-  const isBeforeMandat =
-    new Date(Number(year), Number(month) - 1, Number(day)).getTime() < new Date(2021, 0, 1).getTime();
-  if (isBeforeMandat) {
-    return 'avant 2021';
-  }
-  return `le ${doneAt}`;
-}
+// function getDoneAtText(doneAt: string): string {
+//   const [day, month, year] = doneAt.split('/');
+//   const isBeforeMandat =
+//     new Date(Number(year), Number(month) - 1, Number(day)).getTime() < new Date(2021, 0, 1).getTime();
+//   if (isBeforeMandat) {
+//     return 'avant 2021';
+//   }
+//   return `le ${doneAt}`;
+// }
 
-function getStatusText(status: Status, doneAt?: string): string {
+function getStatusText(status: Status): string {
   const statusText = {
-    done: () => `Terminé (${getDoneAtText(doneAt!)})`,
-    wip: () => 'En travaux',
-    planned: () => 'Prévu',
-    postponed: () => 'Reporté après 2026',
-    variante: () => 'Variante',
-    'variante-postponed': () => 'Variante reportée après 2026',
-    unknown: () => 'Tracé à définir'
+    done: 'Terminé',
+    wip: 'En travaux',
+    planned: 'Prévu',
+    postponed: 'Reporté après 2026',
+    variante: 'Variante',
+    'variante-postponed': 'Variante reportée après 2026',
+    unknown: 'Tracé à définir'
   };
-  return statusText[status]();
+  return statusText[status];
 }
 </script>
