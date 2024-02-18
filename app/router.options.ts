db4a836fc11fd4f1e99df322efe2eb877cc2f341
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-02-18 11:36:06 +0100

    wip

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,3 +1,14 @@
 export default {
-  scrollBehavior: () => ({ left: 0, top: 0, behavior: 'smooth' })
-}
+  scrollBehavior(to: { hash: string }) {
+    if (to.hash) {
+      const navbarHeight = document.getElementById('navigation-header')?.offsetHeight || 0;
+      return {
+        selector: to.hash,
+        offset: { x: 0, y: navbarHeight },
+        behavior: 'smooth'
+      };
+    } else {
+      return { left: 0, top: 0, behavior: 'smooth' };
+    }
+  }
+};
---
+++ components/content/prose/ProseH4.vue
@@ -0,0 +1,21 @@
+<template>
+  <h4 :id="id">
+    <a
+      v-if="generate"
+      :href="`#${id}`"
+      class="not-prose"
+    >
+      <slot />
+    </a>
+    <slot v-else />
+  </h4>
+</template>
+
+<script setup lang="ts">
+import { computed, useRuntimeConfig } from '#imports';
+
+const props = defineProps<{ id?: string }>();
+
+const { headings } = useRuntimeConfig().public.mdc;
+const generate = computed(() => props.id && headings?.anchorLinks?.h4);
+</script>
