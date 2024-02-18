9f89b17775d436d9ae7c44b115204785a9797ced
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-02-18 20:10:22 +0100

    scroll behavior

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,10 +37,13 @@ onBeforeMount(() => {
   const navbarHeight = navbar ? navbar.offsetHeight : 0;
   document.documentElement.style.setProperty('--navbar-height', navbarHeight + 'px');
 });
-
 </script>
 
 <style>
+html {
+  scroll-behavior: smooth;
+}
+
 h1, h2, h3, h4, h5, h6 {
   scroll-margin-top: calc(var(--navbar-height));
 }
