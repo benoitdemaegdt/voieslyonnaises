e7ae8ea0a22c5c0b583f7e6294174bf6cd6e7b5b
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-02-22 08:38:11 +0100

    wip

--- server/routes/content.ts
+++
@@ -1,5 +0,0 @@
-import { serverQueryContent } from '#content/server';
-export default defineEventHandler(async event => {
-  const content = await serverQueryContent(event).where({ _dir: 'voies-lyonnaises', _type: 'markdown' }).find();
-  return { content };
-});
