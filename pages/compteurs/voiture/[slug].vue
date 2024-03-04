559e4cc7de255a8a8d1a7f99913715358d2cf850
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-03-04 23:22:40 +0100

    add compteur voiture page

---
+++ pages/compteurs/voiture/[slug].vue
@@ -0,0 +1,61 @@
+<template>
+  <ContentFrame
+    v-if="counter"
+    header="compteur voiture"
+    :title="counter.name"
+    :description="counter.description"
+    :image-url="counter.imageUrl"
+  >
+    <ClientOnly>
+      <Map :features="features" :options="{ legend: false }" class="mt-12" style="height: 40vh" />
+    </ClientOnly>
+    <h2>Total des passages par année</h2>
+    <p>Ce premier diagramme représente le nombre total de passages détecté par le compteur voiture chaque année.</p>
+    <ChartTotalByYear :data="counter" class="mt-8 lg:p-4 lg:rounded-lg lg:shadow-md" />
+
+    <h2>Comparaison des passages pour un mois donné</h2>
+    <p>Choisissez un mois dans le menu déroulant ci-dessous pour visualiser l'évolution de la fréquentation voiture pour le même mois de chaque année.</p>
+    <ChartMonthComparison :data="counter" class="mt-8 lg:p-4 lg:rounded-lg lg:shadow-md" />
+
+    <template v-if="counter.limitation">
+      <h2>Limitation</h2>
+      <p>{{ counter.limitation }}</p>
+    </template>
+
+    <h2>Source des données</h2>
+    <p>Les données proviennent du <a href="https://avatar.cerema.fr/cartographie" target="_blank">Cerema</a>.</p>
+  </ContentFrame>
+</template>
+
+<script setup>
+const { path } = useRoute();
+const { withoutTrailingSlash } = useUrl();
+const { getCompteursFeatures } = useMap();
+
+const { data: counter } = await useAsyncData(`compteur-${path}`, () => {
+  return queryContent()
+    .where({ _path: withoutTrailingSlash(path) })
+    .findOne();
+});
+
+if (!counter.value) {
+  const router = useRouter();
+  router.push({ path: '/404' });
+}
+
+const features = getCompteursFeatures({ counters: [counter.value] });
+
+const DESCRIPTION = `Compteur voiture ${counter.value.name}`;
+const IMAGE_URL = counter.value.imageUrl;
+useHead({
+  meta: [
+    // description
+    { hid: 'description', name: 'description', content: DESCRIPTION },
+    { hid: 'og:description', property: 'og:description', DESCRIPTION },
+    { hid: 'twitter:description', name: 'twitter:description', DESCRIPTION },
+    // cover image
+    { hid: 'og:image', property: 'og:image', content: IMAGE_URL },
+    { hid: 'twitter:image', name: 'twitter:image', content: IMAGE_URL }
+  ]
+});
+</script>
