a526ffe6735c82abc3c94d00ddb70a62b6850091
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-03-19 08:14:27 +0100

    update check health script

--- .github/scripts/check_data_health.js
+++ .github/scripts/check_data_health.js
@@ -44,8 +44,7 @@ function getAllLinks() {
         const cleanTitle = title
           .replace(/<\/?[^>]+(>|$)/g, '')
           .replace(/\//g, '')
-          .replace(/[(),*']/g, '')
-          .replace(/<br\/>/g, '');
+          .replace(/[(),*']/g, '');
 
         // Replace spaces with hyphens and convert to lower case
         const link = cleanTitle
