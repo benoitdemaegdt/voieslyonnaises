971b69f908da8c2350af10965f52afc77e80fc4d
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-03-23 11:43:26 +0100

    standby sur les absolute

--- components/tooltips/CounterTooltip.vue
+++ components/tooltips/CounterTooltip.vue
@@ -32,21 +32,21 @@
           <div>en moyenne</div>
         </div>
       </div>
-      <div v-if="isAbsoluteBest">
-        <div class="my-2 mx-8 bg-lvv-pink text-sm text-white font-semibold rounded-xl px-1.5 border-black border-2">
-          record absolu
-        </div>
-      </div>
-      <div v-if="isMonthBest && !isAbsoluteBest">
-        <div class="my-2 mx-8 bg-lvv-pink text-sm text-white font-semibold rounded-xl px-1.5 border-white border-2">
-          record mensuel
-        </div>
-      </div>
-      <div v-if="!isMonthBest && !isAbsoluteBest">
-        <div class="my-2 mx-8 text-sm text-white font-semibold rounded-xl px-1.5 border-white border-2">
-          aucun record
-        </div>
-      </div>
+      <!--      <div v-if="isAbsoluteBest">-->
+      <!--        <div class="my-2 mx-8 bg-lvv-pink text-sm text-white font-semibold rounded-xl px-1.5 border-black border-2">-->
+      <!--          record absolu-->
+      <!--        </div>-->
+      <!--      </div>-->
+      <!--      <div v-if="isMonthBest && !isAbsoluteBest">-->
+      <!--        <div class="my-2 mx-8 bg-lvv-pink text-sm text-white font-semibold rounded-xl px-1.5 border-white border-2">-->
+      <!--          record mensuel-->
+      <!--        </div>-->
+      <!--      </div>-->
+      <!--      <div v-if="!isMonthBest && !isAbsoluteBest">-->
+      <!--        <div class="my-2 mx-8 text-sm text-white font-semibold rounded-xl px-1.5 border-white border-2">-->
+      <!--          aucun record-->
+      <!--        </div>-->
+      <!--      </div>-->
     </div>
   </div>
 </template>
@@ -77,10 +77,10 @@ const isFirstMonth = computed(() => countIndex.value === 0);
 const isLastMonth = computed(() => countIndex.value === feature.properties.counts.length - 1);
 const humanDate = computed(() => new Date(count.value.month).toLocaleString('fr-Fr', { month: 'long', year: 'numeric' }));
 const averageDailyTraffic = computed(() => getAverageDailyTraffic(count.value));
-const monthBests = findMonthBests(feature.properties.counts);
-const absoluteBest = findAbsoluteBest(feature.properties.counts)!;
-const isMonthBest = computed(() => monthBests.get(new Date(count.value.month).getMonth()));
-const isAbsoluteBest = computed(() => count.value.month === absoluteBest.month);
+// const monthBests = findMonthBests(feature.properties.counts);
+// const absoluteBest = findAbsoluteBest(feature.properties.counts)!;
+// const isMonthBest = computed(() => monthBests.get(new Date(count.value.month).getMonth()));
+// const isAbsoluteBest = computed(() => count.value.month === absoluteBest.month);
 
 function getAverageDailyTraffic({ month, count }: {
   month: string,
@@ -93,26 +93,26 @@ function getAverageDailyTraffic({ month, count }: {
   return Math.round(averageDailyTraffic / clamp) * clamp;
 }
 
-function findMonthBests(counts: Array<{ month: string; count: number }>) {
-  const monthBests = new Map();
-
-  counts.forEach(count => {
-    const month = new Date(count.month).getMonth();
-
-    if (!monthBests.has(month)) {
-      monthBests.set(month, 0);
-    }
-    if (count.count > monthBests.get(month)) {
-      monthBests.set(month, count.count);
-    }
-  });
-
-  return monthBests;
-}
-
-function findAbsoluteBest(counts: Array<{ month: string; count: number }>) {
-  return counts.toSorted((a, b) => (a.count > b.count ? 1 : a.count < b.count ? -1 : 0)).at(-1);
-}
+// function findMonthBests(counts: Array<{ month: string; count: number }>) {
+//   const monthBests = new Map();
+//
+//   counts.forEach(count => {
+//     const month = new Date(count.month).getMonth();
+//
+//     if (!monthBests.has(month)) {
+//       monthBests.set(month, 0);
+//     }
+//     if (count.count > monthBests.get(month)) {
+//       monthBests.set(month, count.count);
+//     }
+//   });
+//
+//   return monthBests;
+// }
+//
+// function findAbsoluteBest(counts: Array<{ month: string; count: number }>) {
+//   return counts.toSorted((a, b) => (a.count > b.count ? 1 : a.count < b.count ? -1 : 0)).at(-1);
+// }
 
 const changeMonth = (offset: number) => {
   countIndex.value += offset;
