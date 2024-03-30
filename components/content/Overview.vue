9d848f3091c2d6d88d3ad1946e8892c28d10d2b6
Delapouite <delapouite@gmail.com>
2024-03-30 16:20:33 +0100

    feat: improve Overview component

--- components/content/Overview.vue
+++ components/content/Overview.vue
@@ -1,40 +1,11 @@
 <template>
   <div>
     <div>
-      <div class="space-y-6">
-        <dl class="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x">
-          <div class="px-4 py-5 sm:p-6">
-            <dt class="mt-0 text-base font-normal text-gray-900">
-              Distance
-            </dt>
-            <dd class="mt-1 pl-0 flex justify-between items-baseline md:block lg:flex">
-              <div class="flex items-baseline text-2xl font-semibold" :style="`color: ${voie.color}`">
-                {{ Math.round(voie.distance/1000) }}km
-              </div>
-            </dd>
-          </div>
-          <div class="px-4 py-5 sm:p-6">
-            <dt class="mt-0 text-base font-normal text-gray-900">
-              Fréquentation
-            </dt>
-            <dd class="mt-1 pl-0 flex justify-between items-baseline md:block lg:flex">
-              <div class="flex items-baseline text-2xl font-semibold" :style="`color: ${voie.color}`">
-                {{ voie.trafic }}
-              </div>
-            </dd>
-          </div>
-          <div class="px-4 py-5 sm:p-6">
-            <dt class="mt-0 text-base font-normal text-gray-900">
-              Avancement
-            </dt>
-            <dd class="mt-1 pl-0 flex justify-between items-baseline md:block lg:flex">
-              <div class="flex items-baseline text-2xl font-semibold" :style="`color: ${voie.color}`">
-                {{ avancement }}%
-              </div>
-            </dd>
-          </div>
-        </dl>
+      <div class="text-center text-black">
+        Distance totale: <strong>{{ Math.round(voie.distance / 1000) }}km</strong> - Fréquentation max 2030: <strong>{{ voie.trafic }}</strong>
       </div>
+      <ProgressBar :voies="[geojson]" />
+      <Stats :voies="[geojson]" />
     </div>
     <section aria-labelledby="shipping-heading" class="mt-10">
       <ClientOnly>
@@ -46,7 +17,6 @@
 
 <script setup>
 const { path } = useRoute();
-const { getDistance } = useStats();
 
 const { voie } = defineProps({ voie: Object });
 
@@ -65,8 +35,4 @@ const { data: geojson } = await useAsyncData(`geojson-${path}`, () => {
 });
 
 const features = geojson.value.features;
-
-const doneFeatures = features.filter(feature => feature.properties.status === 'done');
-const doneDistance = getDistance({ features: doneFeatures });
-const avancement = Math.round(doneDistance / voie.distance * 100);
 </script>
