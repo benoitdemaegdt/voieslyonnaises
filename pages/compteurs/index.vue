a2cec40a4a3a0d596aeafe2b96e810d6af48bb6a
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-09-29 19:42:43 +0200

    wip

--- pages/compteurs/index.vue
+++ pages/compteurs/index.vue
@@ -6,11 +6,8 @@
     <div class="relative max-w-7xl mx-auto">
       <div class="text-center">
         <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-          Comparaison de la fréquentation cycliste et automobile de certains axes
+          Comparaison de la fréquentation cycliste et automobile.
         </h2>
-        <p class="mt-8 text-xl text-gray-500 leading-8">
-          Lorem Ipsum
-        </p>
         <!-- <ClientOnly>
           <Map :features="features" :options="{ legend: false, filter: false }" class="mt-12" style="height: 40vh" />
         </ClientOnly> -->
@@ -33,50 +30,72 @@
             <thead>
               <tr class="bg-lvv-blue-100">
                 <th class="w-1/6 italic font-normal">
-                  {{ getCounterLastRecord(counter).month }}
+                  {{ getLastRecord(counter).month }}
                 </th>
                 <th class="w-1/4">
-                  {{ getCounterLastRecordPreviousYear(counter).year }}
+                  {{ getLastRecordPreviousYear(counter).year }}
                 </th>
                 <th class="w-1/4">
-                  {{ getCounterLastRecord(counter).year }}
+                  {{ getLastRecord(counter).year }}
                 </th>
                 <th class="w-1/4 italic font-normal border-l-2 border-lvv-blue-600">
                   évolution
                 </th>
               </tr>
             </thead>
-            <tbody>
+            <tbody class="bg-white">
+              <!-- vélo -->
               <tr>
                 <td class="text-center p-1">
-                  <Icon name="game-icons:dutch-bike" class="text-3xl" />
+                  <Icon name="fluent:vehicle-bicycle-16-regular" class="text-3xl" />
                 </td>
                 <td class="text-center p-1">
-                  {{ getCounterLastRecordPreviousYear(counter).value }}
+                  {{ getLastRecordPreviousYear(counter).veloCount }}
                 </td>
                 <td class="text-center p-1">
-                  <div class="flex items-center justify-center">
-                    <span>{{ getCounterLastRecord(counter).value }}</span>
-                    <Icon v-if="isLastRecordMax(counter)" name="iconoir:medal-1st-solid" class="text-lvv-pink text-xl" />
-                  </div>
+                  {{ getLastRecord(counter).veloCount }}
                 </td>
                 <td class="text-center p-1 border-l-2 border-lvv-blue-600">
-                  <span v-if="getEvolution(counter) === 0 " class="text-green-600">
+                  <div v-if="getCounterEvolution(counter).veloEvolution === 0">
                     N/A
-                  </span>
-                  <span v-if="getEvolution(counter) > 0 " class="text-green-600">
+                  </div>
+                  <div v-else-if="getCounterEvolution(counter).veloEvolution > 0" class="text-green-600 flex items-center justify-center">
                     <Icon name="mdi:arrow-top-right-thin" />
-                    +{{ getEvolution(counter) }}%
-                  </span>
-                  <span v-if="getEvolution(counter) < 0 " class="text-red-600">
+                    <span>+{{ getCounterEvolution(counter).veloEvolution }}% </span>
+                  </div>
+                  <div v-else-if="getCounterEvolution(counter).veloEvolution < 0" class="text-red-600 flex items-center justify-center">
                     <Icon name="mdi:arrow-bottom-right-thin" />
-                    {{ getEvolution(counter) }}%
-                  </span>
+                    <span>{{ getCounterEvolution(counter).veloEvolution }}% </span>
+                  </div>
+                </td>
+              </tr>
+              <!-- voiture -->
+              <tr>
+                <td class="text-center p-1">
+                  <Icon name="fluent:vehicle-car-profile-16-regular" class="text-3xl" />
+                </td>
+                <td class="text-center p-1">
+                  {{ getLastRecordPreviousYear(counter).voitureCount }}
+                </td>
+                <td class="text-center p-1">
+                  {{ getLastRecord(counter).voitureCount }}
+                </td>
+                <td class="text-center p-1 border-l-2 border-lvv-blue-600">
+                  <div v-if="getCounterEvolution(counter).voitureEvolution === 0">
+                    N/A
+                  </div>
+                  <div v-else-if="getCounterEvolution(counter).voitureEvolution > 0" class="text-green-600 flex items-center justify-center">
+                    <Icon name="mdi:arrow-top-right-thin" />
+                    <span>+{{ getCounterEvolution(counter).voitureEvolution }}% </span>
+                  </div>
+                  <div v-else-if="getCounterEvolution(counter).voitureEvolution < 0" class="text-red-600 flex items-center justify-center">
+                    <Icon name="mdi:arrow-bottom-right-thin" />
+                    <span>{{ getCounterEvolution(counter).voitureEvolution }}% </span>
+                  </div>
                 </td>
               </tr>
             </tbody>
           </table>
-          </NuxtLink>
         </div>
       </div>
     </div>
@@ -84,7 +103,6 @@
 </template>
 
 <script setup lang="ts">
-const { getCounterLastRecord, getCounterLastRecordPreviousYear, getEvolution, isLastRecordMax } = useCompteur();
 /**
  * la clé cyclopolisId sert à faire le lien entre les compteurs vélo et voiture
  * cette page compare les 2 sur un même axe, on ne s'intéresse donc qu'à ceux qui ont
@@ -102,8 +120,7 @@ type Counter = {
   name: string;
   arrondissement: string;
   coordinates: [number, number];
-  veloCounts: { month: string; count: number }[];
-  voitureCounts: { month: string; count: number }[];
+  counts: { month: string, veloCount: number, voitureCount: number }[];
 };
 
 const counters = allVeloCounters.value?.map(veloCounter => {
@@ -113,9 +130,54 @@ const counters = allVeloCounters.value?.map(veloCounter => {
     name: veloCounter.name,
     arrondissement: veloCounter.arrondissement,
     coordinates: veloCounter.coordinates,
-    veloCounts: veloCounter.counts,
-    voitureCounts: voitureCounter.counts
+    counts: voitureCounter.counts.map((voitureCount: Count) => {
+      const veloCount = veloCounter.counts.find((veloCount: Count) => veloCount.month === voitureCount.month);
+      return {
+        month: voitureCount.month,
+        veloCount: veloCount?.count || 0,
+        voitureCount: voitureCount.count
+      };
+    })
   };
 }).filter((counter): counter is Counter => !!counter) || [];
+
+function getLastRecord(counter: Counter) {
+  const lastRecord = counter.counts.at(-1);
+  return {
+    month: new Date(lastRecord!.month).toLocaleString('fr-Fr', { month: 'short' }),
+    year: new Date(lastRecord!.month).toLocaleString('fr-Fr', { year: 'numeric' }),
+    veloCount: lastRecord!.veloCount,
+    voitureCount: lastRecord!.voitureCount
+  };
+}
+
+/**
+ * permet de comparer le dernier enregistrement, avec celui de l'année précédente
+ */
+function getLastRecordPreviousYear(counter: Counter) {
+  const lastRecord = counter.counts.at(-1);
+  const lastRecordMonth = new Date(lastRecord!.month).getMonth();
+  const lastRecordYear = new Date(lastRecord!.month).getFullYear();
+
+  const lastRecordPreviousYear = counter.counts.find(count => {
+    const countMonth = new Date(count.month).getMonth();
+    const countYear = new Date(count.month).getFullYear();
+    return countMonth === lastRecordMonth && countYear === lastRecordYear - 1;
+  });
+  return {
+    month: new Date(lastRecordPreviousYear!.month).toLocaleString('fr-Fr', { month: 'short' }),
+    year: new Date(lastRecordPreviousYear!.month).toLocaleString('fr-Fr', { year: 'numeric' }),
+    veloCount: lastRecordPreviousYear!.veloCount,
+    voitureCount: lastRecordPreviousYear!.voitureCount
+  };
+}
+
+function getCounterEvolution(counter: Counter) {
+  const lastRecord = getLastRecord(counter);
+  const lastRecordPreviousYear = getLastRecordPreviousYear(counter);
+  const veloEvolution = lastRecord.veloCount === 0 ? 0 : Math.round((lastRecord.veloCount - lastRecordPreviousYear.veloCount) / lastRecord.veloCount * 100);
+  const voitureEvolution = lastRecord.voitureCount === 0 ? 0 : Math.round((lastRecord.voitureCount - lastRecordPreviousYear.voitureCount) / lastRecord.voitureCount * 100);
+  return { veloEvolution, voitureEvolution };
+}
 
 </script>
