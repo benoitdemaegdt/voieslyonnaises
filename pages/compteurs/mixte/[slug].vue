46850893173cfe16b3420d4d78c48ec659168308
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-09-30 12:34:17 +0200

    wip

--- pages/compteurs/mixte/[slug].vue
+++ pages/compteurs/mixte/[slug].vue
@@ -1,19 +1,15 @@
 <template>
   <ContentFrame
     v-if="veloCounter"
-    header="Comparaison fréquentation cycliste et automobile"
+    header="Fréquentation vélo & voiture"
     :title="veloCounter.name"
   >
     <!-- <ClientOnly>
       <Map :features="features" :options="{ legend: false, filter: false }" class="mt-12" style="height: 40vh" />
     </ClientOnly> -->
     <h2>Total des passages par année</h2>
-    <p>Ce premier diagramme représente le nombre total de passages détecté par le compteur vélo chaque année.</p>
-    <!-- <ChartTotalByYear :title="graphTitles.totalByYear" :data="counter" class="mt-8 lg:p-4 lg:rounded-lg lg:shadow-md" /> -->
-
-    <h2>Comparaison des passages pour un mois donné</h2>
-    <p>Choisissez un mois dans le menu déroulant ci-dessous pour visualiser l'évolution de la fréquentation cyclable pour le même mois de chaque année.</p>
-    <!-- <ChartMonthComparison :title="graphTitles.monthComparison" :data="counter" class="mt-8 lg:p-4 lg:rounded-lg lg:shadow-md" /> -->
+    <p>Comparaison de la fréquentation annuelle entre les vélos et les voitures. Ceci est possible grâce à la présence de compteurs automatiques sur les différentes voies.</p>
+    <ChartLine :data="data" :name="veloCounter.name" class="mt-8 lg:p-4 lg:rounded-lg lg:shadow-md" />
 
     <h2>Source des données</h2>
     <p>Les données des compteurs vélo proviennent de <a href="https://data.eco-counter.com/ParcPublic/?id=3902#" target="_blank">data.eco-counter.com</a>.</p>
@@ -21,7 +17,9 @@
   </ContentFrame>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type { Count } from '../../../composables/useCompteur';
+
 const { params } = useRoute();
 
 const { data: veloCounter } = await useAsyncData(() => {
@@ -32,17 +30,19 @@ const { data: voitureCounter } = await useAsyncData(() => {
   return queryContent('compteurs/voiture').where({ cyclopolisId: params.slug }).findOne();
 });
 
-console.log(' ');
-console.log(' ');
-console.log('veloCounter >>', veloCounter.value);
-console.log('voitureCounter >>', voitureCounter.value);
-console.log(' ');
-console.log(' ');
+if (!veloCounter.value || !voitureCounter.value) {
+  const router = useRouter();
+  router.push({ path: '/404' });
+}
 
-// if (!counter.value) {
-//   const router = useRouter();
-//   router.push({ path: '/404' });
-// }
+const data = voitureCounter.value?.counts.map((voitureCount: Count) => {
+  const veloCount = veloCounter.value?.counts.find((veloCount: Count) => veloCount.month === voitureCount.month);
+  return {
+    month: voitureCount.month,
+    veloCount: veloCount?.count || 0,
+    voitureCount: voitureCount.count
+  };
+});
 
 // const graphTitles = {
 //   totalByYear: `Fréquentation cycliste annuelle - ${counter.value.name}`,
