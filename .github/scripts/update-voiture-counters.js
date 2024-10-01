77dac51eb5ea513829ae1f55cf3526c662851328
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2024-10-01 14:46:19 +0200

    update voiture script handles several ids

--- .github/scripts/update-voiture-counters.js
+++ .github/scripts/update-voiture-counters.js
@@ -12,7 +12,7 @@ const path = require('path');
   const trackedCounters = getTrackedCounters();
   for (const { file, counter } of trackedCounters) {
     console.log(`<<<<<<< ${counter.name} >>>>>>>`);
-    const counts = await getCompteurData({ idPdc: counter.idPdc });
+    const counts = await getCompteurData({ idsPdc: counter.idsPdc });
     updateFile({ file, counter: { ...counter, counts } });
   }
 })();
@@ -21,34 +21,93 @@ const path = require('path');
  * Suite aux échanges avec le Cerema, utilisation de l'API aggregated_measures.
  * Elle retourne le volume moyen quotidien sur le mois concené.
  * Pour avoir le volume total mensuel, on multiplie par le nbr de jours dans le mois.
+ *
+ * Cyclopolis raisonne en axe, pas en compteur.
+ * Un compteur au sens cyclopolis peut donc regrouper plusieurs compteurs au sens Cerema.
+ * Typiquement, un compteur cyclopolis est un aggrégat de 2 compteurs Cerema, un pour chaque sens de circulation.
  */
-async function getCompteurData({ idPdc }) {
+async function getCompteurData({ idsPdc }) {
   const startTime = '2018-01-01T00:00:00';
   const endTime = getLastDayOfPreviousMonth();
-  const params = new URLSearchParams({
-    count_point_ids: idPdc,
-    start_time: startTime,
-    end_time: endTime,
-    time_zone: 'Europe/Paris',
-    aggregation_period: 'month',
-    limit: 1000 // default 10
-  });
-  const URL = 'https://avatar.cerema.fr/api/aggregated_measures/?' + params.toString();
-  const res = await fetch(URL);
-  if (res.ok) {
-    const countPoints = await res.json();
-    return countPoints.map(countPoint => {
-      const daysInMonth = getDaysInMonth(countPoint.dt);
-      return {
-        month: countPoint.dt.slice(0, 10),
-        count: Math.round(countPoint.q * daysInMonth)
-      };
+
+  const resByIdPdc = {}
+
+  for (const idPdc of idsPdc) {
+    const params = new URLSearchParams({
+      count_point_ids: idPdc,
+      start_time: startTime,
+      end_time: endTime,
+      time_zone: 'Europe/Paris',
+      aggregation_period: 'month',
+      limit: 1000 // default 10
     });
-  } else {
-    console.log(res.statusText);
-    console.error('[getCompteurData] An error happened while fetching counter data');
-    process.exit(1);
+    const URL = 'https://avatar.cerema.fr/api/aggregated_measures/?' + params.toString();
+    const res = await fetch(URL);
+    if (res.ok) {
+      const countPoints = await res.json();
+
+      resByIdPdc[idPdc] = countPoints.map(countPoint => {
+        const daysInMonth = getDaysInMonth(countPoint.dt);
+        return {
+          month: countPoint.dt.slice(0, 10),
+          count: Math.round(countPoint.q * daysInMonth)
+        };
+      });
+    } else {
+      console.log(res.statusText);
+      console.error('[getCompteurData] An error happened while fetching counter data');
+      process.exit(1);
+    }
   }
+
+  if (idsPdc.length === 1) return resByIdPdc[idsPdc[0]];
+
+  return mergeCountersData(resByIdPdc);
+}
+
+/**
+ * input looks like this
+ * {
+ *   '1927': [
+ *     { month: '2018-01-01', count: 591573 },
+ *     { month: '2018-02-01', count: 538166 },
+ *     { month: '2018-03-01', count: 604185 },
+ *     // other data
+ *   ],
+ *   '1928': [
+ *     { month: '2018-01-01', count: 646861 },
+ *     { month: '2018-02-01', count: 581317 },
+ *     { month: '2018-03-01', count: 682203 },
+ *     // other data
+ *   ]
+ * }
+ *
+ * output should look like
+ * [
+ *   { month: '2018-01-01', count: 1238434 },
+ *   { month: '2018-02-01', count: 1119483 },
+ *   // other data
+ * ]
+ */
+function mergeCountersData(input) {
+  const data = new Map();
+
+  // Iterate through each key in the input object
+  Object.values(input).forEach(dataArray => {
+    dataArray.forEach(item => {
+      const { month, count } = item;
+      if (data.has(month)) {
+        // If the month already exists, add the count
+        data.set(month, data.get(month) + count);
+      } else {
+        // If it's a new month, set the count
+        data.set(month, count);
+      }
+    });
+  });
+
+  // Convert the Map to an array of objects
+  return Array.from(data, ([month, count]) => ({ month, count }));
 }
 
 /**
