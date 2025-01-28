772e1cefba16fdf49b8a64d8cde53182517c3a48
StephaneColson <stephane.colson@protonmail.com>
2025-01-28 10:44:10 +0100

    Add counters for cars

--- .github/scripts/update-velo-counters.js
+++ .github/scripts/update-velo-counters.js
@@ -7,6 +7,8 @@
  */
 const fs = require('fs');
 const path = require('path');
+const dayjs = require('dayjs');
+dayjs().format();
 
 (async () => {
   const allCounters = await getAllCounters();
@@ -75,7 +77,7 @@ async function getUpdatedCounts({ idPdc, flowIds }) {
         idPdc,
         flowIds,
         debut: '01/01/2015',
-        fin: getFirstDayOfCurrentMonth(),
+        fin: dayjs().startOf('month').format('MM/DD/YYYY'),
         interval: '6' // month
       })
   );
@@ -99,25 +101,4 @@ async function getUpdatedCounts({ idPdc, flowIds }) {
 function updateFile({ file, counter }) {
   const filePath = path.join('content/compteurs/velo', file);
   fs.writeFileSync(filePath, JSON.stringify(counter, null, 2));
-}
-
-/**
- * HELPER FUNCTIONS
- */
-function getFirstDayOfCurrentMonth() {
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth();
-
-  // Create a new date object with the year and month, set day to 1
-  const firstDayOfMonth = new Date(year, month, 1);
-
-  // Format the date as "dd/mm/yyyy"
-  return (
-    firstDayOfMonth.getDate().toString().padStart(2, '0') +
-    '/' +
-    (firstDayOfMonth.getMonth() + 1).toString().padStart(2, '0') +
-    '/' +
-    firstDayOfMonth.getFullYear()
-  );
 }
